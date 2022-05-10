ef7586ecd3293d38153d158a6d5f778b2f5dbd70
ghchoi0427 <ghchoi0427@gmail.com>
2022-05-11 00:13:45 +0900

    feat: 비동기 client-server 채팅

---
+++ src/main/java/async/AsyncServer.java
@@ -0,0 +1,53 @@
+package async;
+
+import echoserver.Server;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AsyncServer extends Server {
+
+    ServerSocket serverSocket = null;
+    Socket socket = null;
+    List<Socket> sockets = new ArrayList<>();
+    Thread t;
+
+    public void start() {
+        try {
+            serverSocket = new ServerSocket();
+            serverSocket.bind(new InetSocketAddress("localhost", 8080));
+
+            while (true) {
+                System.out.println("[연결 기다림]");
+                socket = serverSocket.accept();
+                sockets.add(socket);
+
+                InetSocketAddress isa = (InetSocketAddress) socket.getRemoteSocketAddress();
+                System.out.println("[연결 수락함] " + isa.getHostName());
+
+                new Thread(new AsyncRead(socket)).start();
+                new Thread(new AsyncWrite(socket)).start();
+
+            }
+        } catch (
+                Exception e) {
+            e.printStackTrace();
+        }
+
+        if (!serverSocket.isClosed()) {
+            try {
+                for (Socket s : sockets) {
+                    s.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
