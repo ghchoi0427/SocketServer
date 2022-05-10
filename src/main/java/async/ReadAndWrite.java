46b5b9d960a85d103c28ba854289de0012ed42c9
ghchoi0427 <ghchoi0427@gmail.com>
2022-05-11 00:25:29 +0900

    feat: 비동기 채팅

---
+++ src/main/java/async/ReadAndWrite.java
@@ -0,0 +1,36 @@
+package async;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class ReadAndWrite implements Runnable {
+    Socket from;
+    Socket to;
+    byte[] bytes = null;
+
+    public ReadAndWrite(Socket from, Socket to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public void run() {
+        try {
+            InputStream is = from.getInputStream();
+            OutputStream os = to.getOutputStream();
+
+            while (true) {
+                bytes = new byte[100];
+                int readByteCount = is.read(bytes);
+                os.write(new String(bytes, 0, readByteCount, StandardCharsets.UTF_8).getBytes(StandardCharsets.UTF_8));
+
+                if (from.isClosed() || to.isClosed()) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
