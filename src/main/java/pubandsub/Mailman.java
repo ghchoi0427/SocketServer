280d986f09633b619df1289ae38fd2a176793667
ghchoi0427 <ghchoi0427@gmail.com>
2022-06-18 23:07:51 +0900

    refactor: if문 뒤집기

--- src/main/java/pubandsub/Mailman.java
+++ src/main/java/pubandsub/Mailman.java
@@ -17,22 +17,20 @@ public class Mailman implements Runnable {
     public void run() {
         while (true) {
             try {
-                Thread.sleep(10);
+                Thread.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            if (topic.getMessageQueue().isEmpty()) {
-                continue;
-            }
+            if (!topic.getMessageQueue().isEmpty()) {
+                try {
+                    bytes = topic.pollMessage().getBytes(StandardCharsets.UTF_8);
 
-            try {
-                bytes = topic.pollMessage().getBytes(StandardCharsets.UTF_8);
-
-                for (Socket s : topic.getSubscribers()) {
-                    s.getOutputStream().write(bytes);
+                    for (Socket s : topic.getSubscribers()) {
+                        s.getOutputStream().write(bytes);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         }
     }
