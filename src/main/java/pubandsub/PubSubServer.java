8c7a20c74ab5ad9b06da8c04b678fbd1486eba3c
ghchoi0427 <ghchoi0427@gmail.com>
2022-06-18 15:51:47 +0900

    feat: PubSubServer 생성

---
+++ src/main/java/pubandsub/PubSubServer.java
@@ -0,0 +1,50 @@
+package pubandsub;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PubSubServer {
+
+    ServerSocket serverSocket = null;
+    Socket socket = null;
+    List<Socket> sockets = new ArrayList<>();
+
+
+    public PubSubServer() {
+    }
+
+    public void start() {
+        try {
+            serverSocket = new ServerSocket();
+            serverSocket.bind(new InetSocketAddress("localhost", 8080));
+
+            while (true) {
+                System.out.println("[연결 기다림]");
+                socket = serverSocket.accept();
+                sockets.add(socket);
+
+                InetSocketAddress isa = (InetSocketAddress) socket.getRemoteSocketAddress();
+                System.out.println("[연결 수락함] " + isa.getHostName());
+
+                new Thread(new Allocator(socket)).start();
+            }
+        } catch (
+                Exception e) {
+            e.printStackTrace();
+        }
+
+        if (!serverSocket.isClosed()) {
+            try {
+                for (Socket s : sockets) {
+                    s.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
