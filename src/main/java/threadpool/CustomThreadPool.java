22fae5694566523474985736949d77bda6c11380
ghchoi <ghchoi0427@gmail.com>
2022-08-05 13:54:39 +0900

    feat: 쓰레드 풀에서 가져와 사용하는 메서드 생성

---
+++ src/main/java/threadpool/CustomThreadPool.java
@@ -0,0 +1,40 @@
+package threadpool;
+
+import threadpool.crawlanddownload.Crawler;
+import threadpool.crawlanddownload.Downloader;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CustomThreadPool implements DownloadImage {
+    private final int nThreads;
+    private final String baseUrl = "https://www.shutterstock.com/search/";
+
+    public CustomThreadPool(int nThreads) {
+        this.nThreads = nThreads;
+    }
+
+    @Override
+    public void run(String keyword, int page) {
+        Crawler crawler = new Crawler();
+        String[] imageSourceArray = crawler.getImageSourceArray(baseUrl + keyword, page);
+
+        ExecutorService service = Executors.newFixedThreadPool(nThreads);
+        int length = imageSourceArray.length;
+
+        for (int index = 0; index < nThreads; index++) {
+            int finalIndex = index;
+            service.submit(() -> {
+                task(imageSourceArray, (length * finalIndex / nThreads), (length * (finalIndex + 1) / nThreads));
+            });
+        }
+
+        service.shutdownNow();
+    }
+
+    void task(String[] sources, int start, int end) {
+        for (int i = start; i < end; i++) {
+            new Downloader("C:\\temp").downloadImage(sources[i]);
+        }
+    }
+}
