fba4180f3aafbc793be6ee2a8fcb2b4901d1753b
ghchoi <ghchoi0427@gmail.com>
2022-08-05 13:51:03 +0900

    feat: 이미지 다운로더 생성

---
+++ src/main/java/threadpool/crawlanddownload/Downloader.java
@@ -0,0 +1,43 @@
+package threadpool.crawlanddownload;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+/**
+ * image downloader
+ * for shutterstock.com
+ */
+public class Downloader {
+
+    private final String path;
+
+    public Downloader(String path) {
+        this.path = path;
+    }
+
+    public void downloadImage(String urlString) {
+        URL url;
+        InputStream in;
+        OutputStream out;
+        try {
+            url = new URL(urlString);
+            in = url.openStream();
+            File file = new File(path, urlString.split("/")[4]);
+            out = new FileOutputStream(file);
+            while (true) {
+                int data = in.read();
+                if (data == -1) {
+                    break;
+                }
+                out.write(data);
+            }
+            in.close();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
